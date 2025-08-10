66e1353fe004f1d4e5cf3b60c5a3be2792ae09cf
NaeemRS <naeem05041996@gmail.com>
2025-08-10 20:25:49 +0500

    issue

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import CTASection from "@/components/home/CTASection";
 import Hero from "@/components/home/Hero";
 import PortfolioSection from "@/components/home/PortfolioSection";
 import ServiceCard from "@/components/home/ServiceCard";
-import TestimonialSection from "@/components/home/TestimonialSection";
-
+ 
   
 export default function Home() {
   return (
